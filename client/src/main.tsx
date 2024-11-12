8acc1897387e533d856a5fbbe1ea377b71bf4688
Homvi <honvedoadamkj@gmail.com>
2024-11-12 18:00:34 +0100

    feat: integrate React Query into the app
    - Added `QueryClient` and `QueryClientProvider` from `@tanstack/react-query`
    - Configured `queryClient` as the context for `RouterProvider`
    - Set `defaultPreloadStaleTime` to 0 for non-stale loader calls with route preload or visit
    - Wrapped `RouterProvider` in `QueryClientProvider` for global React Query support

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
 import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// Create a client
+const queryClient = new QueryClient()
 
 // Set up a Router instance
 const router = createRouter({
   routeTree,
-  defaultPreload: 'intent'
-});
+  context: {
+    queryClient,
+  },
+  defaultPreload: 'intent',
+  // Since we're using React Query, we don't want loader calls to ever be stale
+  // This will ensure that the loader is always called when the route is preloaded or visited
+  defaultPreloadStaleTime: 0,
+})
 
 // Register things for typesafety
 declare module '@tanstack/react-router' {
@@ -20,6 +30,10 @@ declare module '@tanstack/react-router' {
 const rootElement = document.getElementById('app')!;
 
 if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(<RouterProvider router={router} />);
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>,
+  )
 }
