200a3353cab5de897d99173a8839c0f7a7605f08
Kris Oldrini <kris.oldrini@email.com>
2024-11-22 14:44:16 +0000

    implement a functional popup

--- client/types/zones.ts
+++ client/types/zones.ts
@@ -1,34 +1,40 @@
+// each zone feature in the array is composed of:
 export interface ZoneFeature {
   type: string;
   geometry: {
     type: string;
     coordinates: [number, number]; // [longitude, latitude]
   };
-  properties: {
-    id: number;
-    title: string;
-    description: string;
-    startDate: string;
-    endDate: string;
-    status: string;
-    distance: number;
-    venue: Venue;
-    createdAt: string;
-    updatedAt: string;
-  };
+  properties: Event;
 }
 
+export interface Event {
+  id: number;
+  title: string;
+  description: string;
+  startDate: string;
+  endDate: string;
+  status: string;
+  distance: number;
+  venue: Venue;
+  createdAt: string;
+  updatedAt: string;
+}
+
+// venue displays:
 export interface Venue {
   id: number;
   name: string;
   address: string;
 }
 
+// zone center coordinates
 export interface Center {
   type: string;
   coordinates: [number, number];
 }
 
+//  zone contains an array of features (events), a center and a radius that defines how big this zone is
 export interface Zone {
   type: string;
   features: ZoneFeature[];
@@ -36,8 +42,9 @@ export interface Zone {
   radius: number; //in kilometres
 }
 
+// response from database
 export interface ZoneResponse {
-  status: string; // sucess or error
+  status: string; // success or error
 
   data: {
     type: string;
