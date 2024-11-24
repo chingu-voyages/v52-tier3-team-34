80fe44801c3ccab5d37fa9c076ed127627e581fa
Homvi <honvedoadamkj@gmail.com>
2024-11-24 13:22:24 +0100

    refactor(routes): use AddEvent component in "add-event" route
    Updated `/_protect/dashboard/add-event` route to use the `AddEvent` page component instead of a hardcoded placeholder. Imported `AddEvent` from `client/src/pages/AddEvent.tsx`.

--- client/src/routes/_protect/dashboard/add-event.tsx
+++ client/src/routes/_protect/dashboard/add-event.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import { createFileRoute } from '@tanstack/react-router';
+import AddEvent from '../../../pages/AddEvent';
 
 export const Route = createFileRoute('/_protect/dashboard/add-event')({
   component: RouteComponent
 });
 
 function RouteComponent() {
-  return 'Hello /_protect/dashboard/add-event!';
+  return <AddEvent />;
 }
