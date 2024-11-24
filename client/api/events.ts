a25d1b2968992e133e930216e68e8052f2f244d9
Homvi <honvedoadamkj@gmail.com>
2024-11-24 19:48:09 +0100

    feat(client): add API function to create events
    Added a `createEvent` function in `client/api/events.ts` for sending POST requests to create new events. The function uses `fetch` to send event data to the server and throws an error if the request fails.

---
+++ client/api/events.ts
@@ -0,0 +1,21 @@
+import { serverBaseUrl } from '../config';
+import { EventSubmissionData } from '../src/validations/eventValidation';
+
+// POST
+export const createEvent = async (data: EventSubmissionData) => {
+  console.log('Creating event with: ', data);
+
+  const response = await fetch(`${serverBaseUrl}/events`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to create event');
+  }
+
+  return response.json();
+};
