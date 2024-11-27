196fc3b6aef87726ea9d8c0e8594e0567aadabe0
Homvi <honvedoadamkj@gmail.com>
2024-11-27 18:20:00 +0100

    refactor: update import route

--- client/api/venues.ts
+++ client/api/venues.ts
@@ -1,5 +1,5 @@
 import { serverBaseUrl } from '../config';
-import { FetchVenuesOptions, VenueFormData, VenuesResponse } from '../types/venues';
+import { FetchVenuesOptions, VenueFormData, VenuesResponse } from '../src/types/venues';
 
 // GET ALL
 export const fetchVenues = async ({
