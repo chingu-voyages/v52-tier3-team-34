49f7a1400ed9ee667fc4ee2d4bc67158287540e7
Homvi <honvedoadamkj@gmail.com>
2024-11-27 18:27:13 +0100

    refactor(types): move to src/types

--- client/types/user.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-  id: number;
-  email: string;
-  name: string;
-  googleId: string;
-  profileImage: string | null | undefined;
-  createdAt: string; // datetime
-  updatedAt: string; // datetime
-}
