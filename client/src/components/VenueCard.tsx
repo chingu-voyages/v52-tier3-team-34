6764f7965c53c5db7927978c044bfe1978642aff
Homvi <honvedoadamkj@gmail.com>
2024-11-29 19:27:38 +0100

    feat(venue-card): enhance VenueCard with event filtering and selected state styling

--- client/src/components/VenueCard.tsx
+++ client/src/components/VenueCard.tsx
@@ -2,10 +2,23 @@ import { Venue } from '@/types/venues';
 
 interface VenueCardProps {
   venueInfo: Venue;
+  filterEventsByVenueId: (venueId: number) => void;
+  isSelected?: boolean;
 }
-const VenueCard = ({ venueInfo }: VenueCardProps) => {
+const VenueCard = ({ venueInfo, filterEventsByVenueId, isSelected = false }: VenueCardProps) => {
   return (
-    <div className="border-2 p-1">
+    <div
+      className={`
+      cursor-pointer 
+      p-3 
+      border 
+      rounded-md 
+      transition-all 
+      ${isSelected ? 'bg-neutral-200 border-neutral-600 shadow-xl' : 'hover:bg-gray-50'}
+    `}
+      onClick={() => filterEventsByVenueId(venueInfo.id)}
+    >
+      <p>Id: {venueInfo.id}</p>
       <h3>{venueInfo.name}</h3>
     </div>
   );
