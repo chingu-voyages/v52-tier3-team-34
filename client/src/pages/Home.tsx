e6085a98f4238d224fd8a79ab96e71e6e2fecbe9
Homvi <honvedoadamkj@gmail.com>
2024-12-02 14:45:02 +0100

    style: update Home component layout and design with improved styling and new gradient effects

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   const { data, isPending, isError } = useHealth();
 
   return (
-    <div className="min-h-screen flex flex-col items-start justify-center p-6 ">
+    <div className="mt-14 flex flex-col items-start justify-center p-6 ">
       {/* Server Status Indicator */}
       <div className="flex justify-center items-center mb-6">
         <div
@@ -23,19 +23,19 @@ export default function Home() {
       <p className="text-gray-200 mb-10 italic text-lg">"Find your next eargasm before your ears get bored!"</p>
 
       <div className="space-y-6">
-        <div className="space-y-4 w-full">
+        <div className="space-y-4 w-full min-w-48">
           <Link
             to="/map"
             search={{ lat: 41.390205, lng: 2.154007, radius: 20 }}
             className="relative w-full block group"
           >
             {/* Background div with blur and color shift effect */}
-            <div className="absolute -inset-0.5 bg-gradient-to-r from-purple-600 to-pink-600 rounded-xl opacity-75 group-hover:opacity-100 transition-all duration-300 blur-sm group-hover:blur-lg"></div>
+            <div className="absolute -inset-0.5 bg-gradient-to-r rounded-full from-purple-600 to-pink-600  opacity-75 group-hover:opacity-100 transition-all duration-300 blur-sm group-hover:blur-lg"></div>
 
-            <div className="relative flex items-center justify-center bg-gray-900 text-white py-4 rounded-lg hover:bg-transparent transition-colors duration-300 ease-in-out">
+            <div className="relative flex items-center justify-center bg-black/80 py-4 rounded-full hover:bg-transparent transition-colors duration-300 ease-in-out">
               <MapPin className="mr-3 transition-transform group-hover:rotate-12 group-hover:text-pink-400" size={24} />
               <span className="font-semibold tracking-wider group-hover:text-pink-300 transition-colors">
-                Explore Live Music Map
+                Explore Map
               </span>
             </div>
           </Link>
@@ -44,13 +44,13 @@ export default function Home() {
             {/* Background div with blur and color shift effect */}
             <div className="absolute -inset-0.5 bg-gradient-to-r from-emerald-600 to-green-500 rounded-xl opacity-75 group-hover:opacity-100 transition-all duration-300 blur-sm group-hover:blur-lg"></div>
 
-            <div className="relative px-6 flex items-center justify-center bg-gray-900 text-white py-4 rounded-lg hover:bg-transparent transition-colors duration-300 ease-in-out">
+            <div className="relative px-6 flex items-center justify-center bg-black/80 text-green-600 py-4 rounded-lg hover:bg-transparent transition-colors duration-300 ease-in-out">
               <Plus
                 className="mr-3 transition-transform group-hover:rotate-45 group-hover:text-emerald-400"
                 size={24}
               />
               <span className="font-semibold tracking-wider group-hover:text-emerald-300 transition-colors ">
-                Add Your Venue's Event
+                Add Event
               </span>
             </div>
           </Link>
