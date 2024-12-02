af2a0acc68c3a0e471347864cfbbad13c06ce558
Homvi <honvedoadamkj@gmail.com>
2024-12-02 14:46:47 +0100

    feat: add venue details route with loader and error handling

---
+++ client/src/routes/_protect/dashboard/venue.$venueId.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { createFileRoute, ErrorComponent, ErrorComponentProps } from '@tanstack/react-router';
+import VenueDetails from '@/pages/VenueDetails';
+import { fetchVenue } from '@/api/venues';
+import { VenueResponse } from '@/types/venues';
+
+export const Route = createFileRoute('/_protect/dashboard/venue/$venueId')({
+  loader: async ({ params: { venueId } }) => fetchVenue(venueId),
+  errorComponent: VenueErrorComponent,
+  notFoundComponent: () => {
+    return <p>Venue not found</p>;
+  },
+  component: VenueComponent
+});
+
+export function VenueErrorComponent({ error }: ErrorComponentProps) {
+  return <ErrorComponent error={error} />;
+}
+
+function VenueComponent() {
+  const venueResponse: VenueResponse = Route.useLoaderData();
+  return <VenueDetails venueResponse={venueResponse} />;
+}
