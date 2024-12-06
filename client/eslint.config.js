20d5a139708593a868b4b433d23417f2a08d268a
Homvi <honvedoadamkj@gmail.com>
2024-12-06 14:14:40 +0100

    chore: enhance ESLint configuration with import rules
    - Added `eslint-plugin-import` to enforce import order:
      - Grouped imports logically: `builtin`, `external`, `internal`, etc.
      - Ensured alphabetical ordering within groups with blank lines between them.
    - Updated settings for React to automatically detect the version.
    - Configured import resolver to handle `.js`, `.jsx`, `.ts`, `.tsx` extensions.

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -2,6 +2,7 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
+import importPlugin from 'eslint-plugin-import';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -11,15 +12,38 @@ export default [
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
+    plugins: {
+      import: importPlugin
+    },
     settings: {
       react: {
         version: 'detect'
+      },
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx']
+        }
       }
-    }
-  },
-  {
+    },
     rules: {
-      'react/react-in-jsx-scope': 'off'
+      // React Rules
+      'react/react-in-jsx-scope': 'off',
+
+      // Import Order
+      'import/order': [
+        'error',
+        {
+          groups: [
+            ['builtin', 'external'],
+            ['internal', 'parent', 'sibling', 'index']
+          ],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true
+          }
+        }
+      ]
     }
   }
 ];
