475c01adfeede34ed0d50a06653ff6dee2307853
Homvi <honvedoadamkj@gmail.com>
2024-12-11 15:46:01 +0100

    refactor(login): replace custom login form with Google OAuth integration
    - Removed custom login form and associated logic.
    - Added `GoogleLogin` component for user authentication via Google OAuth.
    - Simplified `LoginComponent` by leveraging `handleSuccess` for Google OAuth login.
    - Removed unused `useRouter` and related imports and logic.
    - Ensured seamless redirection for authenticated users to the dashboard.

--- client/src/routes/login.tsx
+++ client/src/routes/login.tsx
@@ -1,91 +1,41 @@
-import { createFileRoute, redirect, useRouter, useRouterState } from '@tanstack/react-router';
-import * as React from 'react';
-import { z } from 'zod';
+import { GoogleLogin, CredentialResponse } from '@react-oauth/google';
+import { createFileRoute, redirect } from '@tanstack/react-router';
 
-import { useAuth } from '@/auth';
-import { sleep } from '@/utils';
-
-const fallback = '/dashboard' as const;
+import { useAuth } from '@/auth/auth';
 
 export const Route = createFileRoute('/login')({
-  validateSearch: z.object({
-    redirect: z.string().optional().catch('')
-  }),
   beforeLoad: ({ context }) => {
     if (context.auth.isAuthenticated) {
-      throw redirect({ to: fallback });
+      throw redirect({ to: '/dashboard' });
     }
   },
   component: LoginComponent
 });
 
 function LoginComponent() {
-  const auth = useAuth();
-  const router = useRouter();
-  const isLoading = useRouterState({ select: (s) => s.isLoading });
-  const navigate = Route.useNavigate();
-  const [isSubmitting, setIsSubmitting] = React.useState(false);
+  const { login, isAuthenticated, user } = useAuth();
 
-  const search = Route.useSearch();
-
-  async function onFormSubmit(evt: React.FormEvent<HTMLFormElement>) {
-    setIsSubmitting(true);
-    try {
-      evt.preventDefault();
-      const data = new FormData(evt.currentTarget);
-      const fieldValue = data.get('username');
-
-      if (!fieldValue) return;
-      const username = fieldValue.toString();
-      await auth.login(username);
-
-      await router.invalidate();
-
-      // This is just a hack being used to wait for the auth state to update
-      // in a real app, you'd want to use a more robust solution
-      await sleep(1);
-
-      await navigate({ to: fallback });
-    } catch (error) {
-      console.error('Error logging in: ', error);
-    } finally {
-      setIsSubmitting(false);
-    }
+  async function handleSuccess(credentialResponse: CredentialResponse) {
+    login(credentialResponse);
   }
 
-  const isLoggingIn = isLoading || isSubmitting;
-
   return (
-    <div className="p-2 grid gap-2 place-items-center">
-      <h3 className="text-xl">Login page</h3>
-      {search.redirect ? (
-        <p className="text-red-500">You need to login to access this page.</p>
-      ) : (
-        <p>Login and start adding events</p>
-      )}
-      <form className="mt-4 max-w-lg" onSubmit={onFormSubmit}>
-        <fieldset disabled={isLoggingIn} className="w-full grid gap-2">
-          <div className="grid gap-2 items-center min-w-[300px]">
-            <label htmlFor="username-input" className="text-sm font-medium">
-              Username
-            </label>
-            <input
-              id="username-input"
-              name="username"
-              placeholder="Enter your name"
-              type="text"
-              className="border rounded-md p-2 w-full text-black"
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            className="bg-blue-500 text-white py-2 px-4 rounded-md w-full disabled:bg-gray-300 disabled:text-gray-500"
-          >
-            {isLoggingIn ? 'Loading...' : 'Login'}
-          </button>
-        </fieldset>
-      </form>
+    <div className="min-h-96 flex items-center justify-center p-4">
+      <div className="w-full max-w-md space-y-8 p-8  transition-all duration-300">
+        <div className="text-center">
+          {isAuthenticated && user?.name && (
+            <div className="text-center space-y-4">
+              <p className="text-xl text-gray-300 font-semibold">You are logged in</p>
+              <p className="text-3xl text-green-600 font-semibold">Access Granted</p>
+              <p className="text-gray-600">Welcome, {user.name}</p>
+            </div>
+          )}
+        </div>
+
+        <div className="flex justify-center">{!user && <GoogleLogin onSuccess={handleSuccess} shape="pill" />}</div>
+      </div>
     </div>
   );
 }
+
+export default LoginComponent;
